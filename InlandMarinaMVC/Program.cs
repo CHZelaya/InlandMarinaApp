94fb951d08854524cad077cb4686447956cbe334
CHZelaya <chzelaya1201@gmail.com>
2024-11-27 21:44:59 -0700

    Added some styling, jumbotron.

--- InlandMarinaMVC/Program.cs
+++ InlandMarinaMVC/Program.cs
@@ -24,7 +24,7 @@ namespace InlandMarinaMVC
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Marina}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
